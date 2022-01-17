0e11960f041b46b8dfc2c0b14f51103f685d35d0
intersoom <sallybig@naver.com>
2022-01-17 16:14:55 +0900

    모의고사

--- 모의고사.js
+++ 모의고사.js
@@ -13,9 +13,10 @@ function solution(answers) {
     const BRule = [2, 1, 2, 3, 2, 4, 2, 5];
     const CRule = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
+    4, 5, 4, 5, 2
+
     answers.forEach(a => {
-        console.log(a)
-        if (aIndex > 5){
+        if (aIndex >= 5){
             aIndex = 0;
 
             if (a == ARule[aIndex]){
@@ -28,7 +29,7 @@ function solution(answers) {
             }
         }
 
-        if (bIndex > 7){
+        if (bIndex >= 8){
             bIndex = 0;
 
             if (a == BRule[bIndex]){
@@ -42,7 +43,7 @@ function solution(answers) {
         }
         
         
-        if (cIndex > 9){
+        if (cIndex >= 10){
             cIndex = 0;
 
             if (a == CRule[cIndex]){
@@ -60,11 +61,40 @@ function solution(answers) {
         cIndex += 1;
     });
 
-    const sArray = [aScore, bScore, cScore];
+    const obj = {1: aScore, 2: bScore, 3:cScore};
 
-    answer = Math.max(...sArray);
+    function sortObject(obj) {
+        var arr = [];
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                arr.push({
+                    'key': prop,
+                    'value': obj[prop]
+                });
+            }
+        }
+        arr.sort(function(a, b) { return a.value - b.value; });
+        
+        return arr; 
+    }
 
-    answer = sArray.indexOf(answer);
+    const sorted = sortObject(obj).reverse();
+    
+    if (sorted[0].value == sorted[1].value){
+        answer.push(Number(sorted[0].key));
+        answer.push(Number(sorted[1].key));
+
+        if (sorted[1].value == sorted[2].value){
+            answer.push(Number(sorted[2].key));
+        }
+    }
+    else{
+        answer.push(Number(sorted[0].key));
+    }
+
+    answer.sort();
 
     return answer;
 }
+
+console.log(solution([4, 5, 4, 5, 2]))
