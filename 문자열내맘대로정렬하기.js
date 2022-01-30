b14c6626a626efd07a21667b0437b45192b5a009
intersoom <sallybig@naver.com>
2022-01-30 20:56:38 +0900

    문자열 내 맘대로 정렬하기

--- 문자열내맘대로정렬하기.js
+++ 문자열내맘대로정렬하기.js
@@ -1,6 +1,7 @@
 function solution(strings, n) {
     let answer = [];
 
+    answer = answer.sort();
     answer = strings.sort(function(a, b){
         if (a[n] < b[n]) {
             return -1;
@@ -9,20 +10,7 @@ function solution(strings, n) {
             return 1;
         }
         else if (a[n] === b[n]){
-            if (a.slice(n + 1) === b.slice(n + 1)){
-                return 0;
-            }
-            else{
-                const ex = [a.slice(n + 1), b.slice(n + 1)];
-                const exS = [a.slice(n + 1), b.slice(n + 1)].sort();
-
-                if (ex[0] !== exS[0]){
-                    return 1;
-                }
-                else {
-                    return -1;
-                }
-            }
+            return 0;
         }
     });
 
