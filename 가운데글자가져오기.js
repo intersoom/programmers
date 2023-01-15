2a680b2fa197e6d06c70026733e1ae357a660e6d
intersoom <sallybig@naver.com>
2023-01-15 11:34:07 +0900

    가운데 글자 가져오기

---
+++ 가운데글자가져오기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  let answer = "";
+  const len = s.length;
+
+  if (s.length % 2 === 0) {
+    answer = s[len / 2 - 1] + s[len / 2];
+  } else {
+    answer = s[Math.floor(len / 2)];
+  }
+
+  return answer;
+}
